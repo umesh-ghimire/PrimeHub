03c292c59c9cb63c881746a8fff5811ec7616ab2
safal bhattarai <safalbhattarai23422@gmail.com>
2025-12-11 10:22:03 +0545

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-// Add this to any route, e.g. in web.php
-Route::get('/test-confirm', function () {
-    return redirect()->route('password.confirm');
-})->middleware(['auth']);
+
 
 require __DIR__.'/auth.php';
