94f10a402a07e04f873765b5584b2ef1656da9c5
prakash-chand-survivor <prakashxchand@gmail.com>
2025-12-28 15:18:59 +0545

    login with fb and password reset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,16 +49,34 @@
 
                         <!-- Password -->
                         <div>
-                            <x-input-label for="password" value="Password" class="text-xs font-medium" />
-                            <x-text-input
-                                id="password"
-                                name="password"
-                                type="password"
-                                required
-                                autocomplete="current-password"
-                                class="mt-1 h-10 text-sm rounded-lg w-full"
-                            />
-                        </div>
+    <x-input-label for="password" value="Password" class="text-xs font-medium" />
+
+    <div class="relative mt-1">
+        <x-text-input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autocomplete="current-password"
+            class="h-10 text-sm rounded-lg w-full pr-10 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        />
+
+        <button
+            type="button"
+            onclick="togglePassword()"
+            class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-blue-600"
+        >
+            <svg id="eye-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            </svg>
+
+            <svg id="eye-slash-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 hidden">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 0 1-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+            </svg>
+        </button>
+    </div>
+</div>
 
                         <!-- Remember Me + Forgot Password -->
                         <div class="flex items-center justify-between text-xs">
@@ -99,4 +117,21 @@
             </div>
         </div>
     </div>
+    <script>
+    function togglePassword() {
+        const input = document.getElementById('password');
+        const eye = document.getElementById('eye-icon');
+        const eyeSlash = document.getElementById('eye-slash-icon');
+
+        if (input.type === 'password') {
+            input.type = 'text';
+            eye.classList.add('hidden');
+            eyeSlash.classList.remove('hidden');
+        } else {
+            input.type = 'password';
+            eye.classList.remove('hidden');
+            eyeSlash.classList.add('hidden');
+        }
+    }
+</script>
 </x-guest-layout>
